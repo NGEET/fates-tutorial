0f186cf115f87a401a30bfc74459c159e93a5e03
Jessica Needham <JFNeedham@38-f9-d3-9d-30-37.dhcp.lbnl.us>
2024-01-19 14:21:27 -0800

    update script for cohort and patch files

---
+++ tools/make_inventory_init_files.R
@@ -0,0 +1,76 @@
+#############################################################################
+#### This script reads in BCI data from dryad and converts it to
+#### a format that can be read in by FATES for inventory initialisation #####
+
+# 1. Load R data table  
+# 2. Make patches 
+#    - Assign patch age, area, and land use type
+# 3. Make cohorts
+#    - Assign cohorts to patches
+#    - Assign PFTs to species
+#    - Remove dead trees
+#    - Combine individuals into cohorts
+#############################################################################
+rm(list=ls())
+
+#### Libraries ####
+library(stringi)
+
+#############################################################################
+#### 1. Load Data ####
+# Load the plot data
+load('/Users/JFNeedham/fates-tutorial/inventory_data/bci.tree8.rdata')
+df_full = bci.tree8  
+
+#### 2. Remove dead trees ####
+df = df_full[df_full$status == 'A', ]
+
+# plot area 
+plot_area = 50
+
+# plot name
+plot_name = 'BCI'
+# plot year
+plot_year = as.numeric(stri_sub(df$ExactDate[1], from = 1, to = 4))
+
+# units - conversion for dbh column - use the below values: 
+# cm - 1
+# mm - 0.1
+# m - 100
+units = 0.1
+
+#############################################################################
+#### 3. Make patches ####
+npatches = length(unique(df$quadrat))
+# when was each patch measured - this isn't used by FATES so the year of the census is fine
+time = rep(plot_year, npatches)
+patch_df = as.data.frame(time)
+
+# unique patch code
+patch_df$patch = unique(df$quadrat)
+
+# is this patch secondary or primary - 
+# here we assume  primary for all subplots, but if  this information is available  then change
+# the lines below
+# 0  = non forest, 1 = secondary, 2 = primary
+patch_df$trk = rep(2, npatches)
+
+# time since the patch was created - can be set to 0
+patch_df$age = rep(0, npatches)
+
+# fraction of the site occupied by the patch
+patch_df$area = rep((1/npatches), npatches)
+
+write.csv(patch_df, sprintf('/Users/JFNeedham/fates-tutorial/inventory_data/%s_%i.pss', plot_name, plot_year))
+
+#############################################################################
+#### 3. Make cohorts #### 
+time = rep(plot_year, nrow(df))
+co_df = as.data.frame(time)
+co_df$patch = df$quadrat
+co_df$dbh = df$dbh  * units # convert from cm to mm
+co_df$height = -3
+co_df$pft = 1
+co_df$n = 1/plot_area
+
+write.csv(co_df, sprintf('/Users/JFNeedham/fates-tutorial/inventory_data/%s_%i.css', plot_name, plot_year))
